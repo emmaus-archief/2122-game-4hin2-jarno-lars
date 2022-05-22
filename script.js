292d673d5b644c01ac22916f7838a9010a1bd857
jarno4hf <93325168+jarno4hf@users.noreply.github.com>
2022-05-22 13:55:36 +0000

    game over bijna

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ yoyoyo
 
 const SPELEN = 1;
 const GAMEOVER = 2;
+const UITLEG = 3;
 var spelStatus = SPELEN;
 const LEY_LEFT = 37
 const KEY_RIGHT = 39
@@ -37,29 +38,29 @@ var speler2Y = 620;
 var beweegAlles = function () {
   // speler
   if (keyIsDown(LEFT_ARROW)) {
-speler1X = speler1X -1.2
+speler1X = speler1X -1.6
   }
   if (keyIsDown(RIGHT_ARROW)) {
-speler1X = speler1X +1.2
+speler1X = speler1X +1.6
   }
   if (keyIsDown(DOWN_ARROW)) {
-speler1Y = speler1Y +1.2
+speler1Y = speler1Y +1.6
   }
   if (keyIsDown(UP_ARROW)) {
-speler1Y = speler1Y -1.2
+speler1Y = speler1Y -1.6
   }
   // vijand (speler2)
   if (keyIsDown(65)) {
-speler2X = speler2X -1.5
+speler2X = speler2X -1.8
   }
   if (keyIsDown(68)) {
-speler2X = speler2X +1.5
+speler2X = speler2X +1.8
   }
   if (keyIsDown(83)) {
-speler2Y = speler2Y +1.5
+speler2Y = speler2Y +1.8
   }
   if (keyIsDown(87)) {
-speler2Y = speler2Y -1.5
+speler2Y = speler2Y -1.8
   }
   // kogel
 };
@@ -119,6 +120,13 @@ rect(speler2X-25,speler2Y-25,50,50);
  * anders return false
  */
 var checkGameOver = function () {
+  if (speler1X - speler2X <50 &&
+    speler1X - speler2X > -50 &&
+   speler1Y - speler2Y <50 &&
+   speler1Y - speler2Y > -50) {
+    
+    return true;
+  }
   // check of HP 0 is , of tijd op is, of ...
   return false;
 };
@@ -156,6 +164,13 @@ function draw() {
   }
   if (spelStatus === GAMEOVER) {
     // teken game-over scherm
-
+console.log("game over");
+    textSize(20);
+    fill("white");
+    text("game over", 100, 100)
+  }
+  if (spelStatus === UITLEG) {
+    // teken uitleg scherm
+console.log("uitleg");
   }
 }
